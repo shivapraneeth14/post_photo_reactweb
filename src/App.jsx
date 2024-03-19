085225a9e2c98a9125f99c15dc40a6e4bd87eab2
shivapraneeth14 <shivakandala43@gmail.com>
2024-03-19 21:00:25 +0530

    message

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Components/Header'
-import { Outlet } from 'react-router-dom'
 import { photoinfo,postinfo } from './Data/Datainfo'
 
 function App() {
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Link} from "react-router-dom"
+import { photoinfo, postinfo } from '../Data/Datainfo'
+
 
 function Header() {
+  const [alldata,setalldata] = useState([])
+  const [searchdata,setsearchdata] = useState([])
+  const [searchtearm,setsearchterm] = useState([])
+  useEffect(()=>{
+    async function fetchdata(){
+      try {
+        const postdata = await postinfo();
+        const photodata = await photoinfo();
+        console.log("postdata",postdata)
+        console.log("photodata",photodata)
+        const mergeddata =[...postdata,...photodata]
+        console.log("mergeddata",mergeddata)
+        setalldata(mergeddata)
+      } catch (error) {
+        console.log(error)
+        
+      }
+    }
+    fetchdata()
+  },[])
+  useEffect(()=>{
+    const filtereddata = alldata.filter((data)=>(
+      data.title.toLowerCase().includes(searchtearm)
+    ))
+    setsearchdata(filtereddata)
+  },[searchtearm])
+
+ 
+
+
   return (
     <div>
       <div className=' rounded-xl text-white pt-4 bg-black w-full h-24 flex justify-around'>
        <Link to="/">
        home
        </Link>
-       <input className=' rounded-xl h-8' type="text" />
+       <div className=' z-50'>
+       <input 
+       onChange={(e)=>setsearchterm(e.target.value)}
+       placeholder='search'
+       className=' w-96 text-black rounded-xl h-8' type="text" />
+       {searchtearm &&  searchdata.slice(0, 10).map((data,index)=>(
+         <div  className='  border border-b-black text-xs  w-96 h-7 rounded-xl bg-white text-black ' key={index}>{data.title}</div>
+       ))}
+        
+       </div>
+     
        <Link to="/Posts">
      posts
        </Link>
